fb4bf7f5105caa1b5c7b88d1706c265f32978dc6
Jenny K <jkam17@gmail.com>
2018-09-15 23:24:09 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "select characters.species, count(characters.species) as num 
+  from characters
+  order by num desc
+  limit 1"
 end
 
 def select_name_and_series_subgenres_of_authors
