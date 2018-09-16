edfdf908345d3eb0d99ea2648692ab822d77bf2e
Jenny K <jkam17@gmail.com>
2018-09-16 00:02:55 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,13 +28,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "select series.title
-  from (
+  "
     select series.title, count(characters.species) as num
     from series join characters on series.id = characters.series_id
     group by series.title
     order by num desc
-    limit 1) "
+    limit 1 "
 end
 
 def select_character_names_and_number_of_books_they_are_in
