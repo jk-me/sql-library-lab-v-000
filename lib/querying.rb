87c2baef520d5210a08749b4d605f5fdb10581d7
Jenny K <jkam17@gmail.com>
2018-09-16 00:09:41 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,6 +38,6 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "select characters.name, count(character_books.book_id)
-  from characters join character_books on characters.id = character_books.character_id
+  from characters join character_books on character_books.character_id = characters.id
   group by character_books.character_id"
 end
